291f1b7de243d04ab60db93e7c0e9845870667dd
w68140 <bohdandviborodchyn@gmail.com>
2023-05-16 09:18:21 +0200

    Lab8

---
+++ Lab8/lista_jednokierunkowa.py
@@ -0,0 +1,77 @@
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+            if previous == None:
+                self.head = current.getNext()
+            else:
+                previous.setNext(current.getNext())
+
+
+myList = UnorderedList()
+myList.add(12)
+myList.add(1)
+myList.add(1154)
+myList.add(184)
+myList.add(135)
+myList.add(16)
+print(myList.size())
+print(myList.search(3))
+print(myList.search(16))
+myList.remove(12)
+print(myList.size())
